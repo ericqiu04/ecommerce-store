c4eae92ea78482773776b48fd7049965e5d7f9ee
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-09-05 01:47:16 -0400

    added pictures to the categories

--- src/components/category.component.jsx
+++ src/components/category.component.jsx
@@ -5,7 +5,8 @@ const Category = () => {
       <div className="categories-container">
         {categories.map((category) => (
           <div key={category.id} className="category-container">
-          <div className = "category-background-image"/>
+          <div className = "background-image" style = {{backgroundImage: `url(${category.imageUrl})`}}/>
+
             <div className="category-body-container">
               <h2>{category.title}</h2>
               <p>Shop Now</p>
