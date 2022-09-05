5ca6d12a02dd19366a6be7de201386cacef524f2
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-09-05 00:47:24 -0400

    added categories file and mapped it into the main app file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
+import categories from "./categories";
+
 const App = () => {
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map((category) => (
+        <div key={category.id} className="category-container">
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-        <div className = "category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-
-      </div>
+      ))}
+      ;
     </div>
   );
 };
---
+++ src/categories.js
@@ -0,0 +1,25 @@
+const categories = [
+
+    {
+        id: 1,
+        title: 'Hats',
+    },
+    {
+        id: 2,
+        title:'Sneakers' 
+    },
+    {
+        id: 3,
+        title: 'Jackets'
+    },
+    {
+        id: 4,
+        title: 'Womens'
+    },
+    {
+        id: 5,
+        title: 'Mens'
+    }
+]
+
+export default categories;
