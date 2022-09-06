82bc62c14cb261a09ed0090529c180a9bc4b2a72
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-09-06 00:59:17 -0400

    added fonts onto the website

--- src/components/category.component.jsx
+++ src/components/category.component.jsx
@@ -1,21 +1,24 @@
 import categories from "../js-files/categories";
-import '../styles/categories.styles.scss';
+import "../styles/categories.styles.scss";
 const Category = () => {
-    return (
-      <div className="categories-container">
-        {categories.map((category) => (
-          <div key={category.id} className="category-container">
-          <div className = "background-image" style = {{backgroundImage: `url(${category.imageUrl})`}}/>
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <div key={category.id} className="category-container">
+          <div
+            className="background-image"
+            style={{ backgroundImage: `url(${category.imageUrl})` }}
+          />
 
-            <div className="category-body-container">
-              <h2>{category.title}</h2>
-              <p>Shop Now</p>
-            </div>
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
           </div>
-        ))}
-        ;
-      </div>
-    );
-  };
-  
-  export default Category;
+        </div>
+      ))}
+      ;
+    </div>
+  );
+};
+
+export default Category;
