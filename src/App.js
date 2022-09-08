bf924c51a109f2f5c5dc707c58ef00b17d9d986b
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-09-07 23:20:10 -0400

    added shop route and learned about nesting structure (will be implemented in the near future)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './routes/home.component'
+import Shop from './routes/shop.component'
 import {Routes, Route} from 'react-router-dom';
 const App = () => {
   return (
     <Routes>
     <Route path = '/' element = {<Home/>}/>
+    <Route paht = '/shop' element = {<Shop/>} />
     </Routes>
       
   );
