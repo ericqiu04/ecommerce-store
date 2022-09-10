3c4c7cd6e59c814153f77f8d0295f8b8b0a0f4b1
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-09-10 15:38:57 -0400

    added logo and css to navigation bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,12 @@
 import { Outlet, Link } from "react-router-dom";
 import {Fragment} from 'react';
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg' 
+import '../../styles/navigation.styles.scss'
 const Navigation = () => {
   return (
     <Fragment>
       <div className = "navigation">
-        <Link className = "logo-container" to = '/'>LOGO</Link>
+        <Link className = "logo-container" to = '/'><CrownLogo className = "logo"/></Link>
         <div className = 'links-container'>
             <Link className = 'nav-links-container' to = '/shop'>
                 SHOP
